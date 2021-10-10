933f859680debfb5507ee179cb6420e30289d974
Birkhoff <birkhoffcheng@gmail.com>
2021-10-10 17:37:48 -0500

    Complete part 2 circle generation algorithm
    Use least square fit to compute the circle center and radius

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -111,5 +111,54 @@ double ofApp::distance(int x1, int y1, int x2, int y2) {
 }
 
 void ofApp::generate_circle() {
+	int i, second_last_blue = 0, last_blue = 0;
+	double Suu = 0, Suv = 0, Suuu = 0, Suvv = 0, Svv = 0, Svvv = 0, Svuu = 0, average_x = 0, average_y = 0, total_blues = 0, u, v, uc, vc, rhs1, rhs2, determinant;
+	for (i = 0; i < NUM_POINTS * NUM_POINTS; i++) {
+		if (square_blue[i]) {
+			total_blues += 1;
+			average_x += squares[i].x;
+			average_y += squares[i].y;
+			second_last_blue = last_blue;
+			last_blue = i;
+		}
+	}
+	average_x /= total_blues;
+	average_y /= total_blues;
 
+	if (total_blues == 2) {
+		circle_center_x = average_x + 5 * SCALE;
+		circle_center_y = average_y + 5 * SCALE;
+		blue_circle_radius = distance(squares[second_last_blue].x, squares[second_last_blue].y, squares[last_blue].x, squares[last_blue].y) / 2.0;
+		return;
+	}
+
+	for (i = 0; i < NUM_POINTS * NUM_POINTS; i++) {
+		if (square_blue[i]) {
+			u = squares[i].x - average_x;
+			v = squares[i].y - average_y;
+			Suu += u * u;
+			Suv += u * v;
+			Suuu += u * u * u;
+			Suvv += u * v * v;
+			Svv += v * v;
+			Svvv += v * v * v;
+			Svuu += v * u * u;
+		}
+	}
+
+	rhs1 = (Suuu + Suvv) / 2.0;
+	rhs2 = (Svvv + Svuu) / 2.0;
+	determinant = Suu * Svv - Suv * Suv;
+	if (determinant != 0) {
+		uc = (Svv * rhs1 - Suv * rhs2) / determinant;
+		vc = (Suu * rhs2 - Suv * rhs1) / determinant;
+	}
+	else {
+		uc = 0;
+		vc = 0;
+	}
+
+	circle_center_x = uc + average_x + 5 * SCALE;
+	circle_center_y = vc + average_y + 5 * SCALE;
+	blue_circle_radius = sqrt(uc * uc + vc * vc + (Suu + Svv) / total_blues);
 }
